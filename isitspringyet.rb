ca94a6e02184a032bf99681d1bacde9722216a34
Randy Schmidt <randy@umlatte.com>
2009-02-25 16:05:25 -0500

    Added in geocoding of ip address

--- isitspringyet.rb
+++ isitspringyet.rb
@@ -1,6 +1,19 @@
 require 'rubygems'
 require 'sinatra'
+require 'net/http'
 
 get '/' do
-  "oh hai"
+  remote_ip = env['REMOTE_ADDR']
+  # remote_ip = '208.75.85.206'
+  begin
+    Timeout::timeout(3) do
+      @latlng = Net::HTTP.get "tinygeocoder.com", "/create-api.php?q=#{remote_ip}"
+    end
+  rescue Timeout::Error
+    "there was a problem"
+  end
+  @lat, @lng = *@latlng.split(",")
+  @hemisphere = @lat.to_f > 0 ? 'northern' : 'southern'
+  erb :index
+  
 end
